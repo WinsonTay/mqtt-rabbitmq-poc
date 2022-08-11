68098bb6231ff98d9ae85259b27f958e70378627
Winson Tay <weeshen90@gmail.com>
2022-08-11 13:20:00 +0800

    init commit.

---
+++ MQTT-Publisher/MQTT-Publisher/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+namespace MQTT_Publisher
+{
+    class Publisher
+    {
+        static async Task Main(string[] args)
+        {
+            var cnt = 0;
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+
+
+            while (cnt < 50)
+            {
+                while (!client.IsConnected && cnt < 50)
+                {
+                    try
+                    {
+                        var options = new MqttClientOptionsBuilder()
+                               .WithClientId(Guid.NewGuid().ToString())
+                               .WithTcpServer("192.168.110.183", 1883)
+                               .WithCredentials("admin", "admin")
+                               .WithCleanSession()
+                               .Build();
+                        await client.ConnectAsync(options);
+
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        Console.WriteLine("MQTT Broker Connection Failed");
+                        await Task.Delay(TimeSpan.FromSeconds(8));
+                    }
+                }
+                while (client.IsConnected && cnt < 50)
+                {
+                    Console.WriteLine("Broker Connected");
+                    await PublishMessageAsync(client);
+                    Console.WriteLine("Message Published");
+                    await Task.Delay(500);
+                    cnt++;
+                }
+
+
+            }
+            await client.DisconnectAsync();
+
+
+        }
+        private static async Task PublishMessageAsync(IMqttClient client)
+        {
+            string MessagePayload = JsonSerializer.Serialize(new MessagePayload()
+            {
+                Value = RandomNumber(50.3, 255.4).ToString(),
+                CreatedAt = DateTime.Now,
+
+            });
+            var message = new MqttApplicationMessageBuilder()
+                               .WithTopic("tag/temperature")
+                               .WithPayload(MessagePayload)
+                               .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                               .Build();
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+                Console.WriteLine(MessagePayload);
+            }
+        }
+        private static double RandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+
+    }
+
+    class MessagePayload
+    {
+        public string Value { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ MQTT-Subscriber/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Collections.Generic;
+using MQTTnet.Protocol;
+using MQTTnet.Packets;
+using System.Text;
+
+
+namespace MQTT_Subscriber
+{
+    public class Subscriber
+    {
+    
+        static async Task Main(string[] args)
+        {
+
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                               .WithClientId(Guid.NewGuid().ToString())
+                               .WithTcpServer("192.168.0.110", 1883)
+                               .WithCredentials("admin", "admin")
+                               .WithCleanSession()
+                               .Build();
+            client.DisconnectedAsync += Client_DisconnectedAsync;
+            client.ApplicationMessageReceivedAsync += Client_ApplicationMessageReceivedAsync;
+
+            while (true)
+            {
+                while (!client.IsConnected)
+                {
+                    try
+                    {
+                        await client.ConnectAsync(options);
+                        var topicFilter = new MqttTopicFilterBuilder()
+                           .WithTopic("tag/temperature")
+                           .Build();
+                        await client.SubscribeAsync(topicFilter);
+                        Console.WriteLine("Broker Connected, Waiting for Publisher Message");
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Connect Broker Failed");
+                        await Task.Delay(8000);
+                    }
+                }
+                await Task.Delay(5000);
+            }
+            
+            await client.DisconnectAsync();
+
+        }
+
+        private static Task Client_DisconnectedAsync(MqttClientDisconnectedEventArgs arg)
+        {
+            Console.WriteLine("Broker Connection Down .. ");
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_ApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
+        {
+            Console.WriteLine("Msg Received");
+            try
+            {
+                    var msg = JsonSerializer.Deserialize<LiveTagMsgPayloadDto>(Encoding.UTF8.GetString(arg.ApplicationMessage.Payload));
+                    Console.WriteLine($"{msg.EdgeId}");
+                    msg.TagPayload.ForEach(t => Console.WriteLine($"Id:{t.Id}, Value:{t.Value}")); 
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("Failed to Serialize");
+                Console.WriteLine(ex.Message);
+            }
+       
+            return Task.CompletedTask;
+        }
+
+    }
+    class MessagePayload
+    {
+        public string Value { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+    public class LiveTagMsgPayloadDto
+    {
+        public string EdgeId { get; set; }
+        public List<TagPayload> TagPayload {get; set;}
+    }
+        public class TagPayload
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+        public string Unit { get; set; }
+    }
+}
